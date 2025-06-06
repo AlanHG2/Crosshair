e8992990a4b22d491cce327d70a56612f699ab95
AlanHG2 <alanhazelgg@gmail.com>
2025-06-06 16:06:55 -0600

    First commit

---
+++ Crosshair/App.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.Gaming.XboxGameBar;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+namespace Crosshair
+{
+    sealed partial class App : Application
+    {
+        private XboxGameBarWidget widgetCrosshair = null;
+        /// <summary>
+        /// Inicializa el objeto de aplicación Singleton. Esta es la primera línea de código creado
+        /// ejecutado y, como tal, es el equivalente lógico de main() o WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            XboxGameBarWidgetActivatedEventArgs widgetArgs = null;
+            if (args.Kind == ActivationKind.Protocol)
+            {
+                var protocolArgs = args as IProtocolActivatedEventArgs;
+                string scheme = protocolArgs.Uri.Scheme;
+                if (scheme.Equals("ms-gamebarwidget"))
+                {
+                    widgetArgs = args as XboxGameBarWidgetActivatedEventArgs;
+                }
+            }
+            if (widgetArgs != null)
+            {
+                if (widgetArgs.IsLaunchActivation)
+                {
+                    var rootFrame = new Frame();
+                    rootFrame.NavigationFailed += OnNavigationFailed;
+                    Window.Current.Content = rootFrame;
+
+                    // Create Game Bar widget object which bootstraps the connection with Game Bar
+                    widgetCrosshair = new XboxGameBarWidget(
+                        widgetArgs,
+                        Window.Current.CoreWindow,
+                        rootFrame);
+                    rootFrame.Navigate(typeof(MainPage));
+                    Window.Current.Closed += Widget1Window_Closed;
+                    Window.Current.Activate();
+                    CenterWidgetOnLaunch();
+                }
+            }
+        }
+        private void Widget1Window_Closed(object sender, Windows.UI.Core.CoreWindowEventArgs e)
+        {
+            widgetCrosshair = null;
+            Window.Current.Closed -= Widget1Window_Closed;
+        }
+
+        /// <summary>
+        /// Se invoca cuando la aplicación la inicia normalmente el usuario final. Se usarán otros puntos
+        /// de entrada cuando la aplicación se inicie para abrir un archivo específico, por ejemplo.
+        /// </summary>
+        /// <param name="e">Información detallada acerca de la solicitud y el proceso de inicio.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // No repetir la inicialización de la aplicación si la ventana tiene contenido todavía,
+            // solo asegurarse de que la ventana está activa.
+            if (rootFrame == null)
+            {
+                // Crear un marco para que actúe como contexto de navegación y navegar a la primera página.
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                // Poner el marco en la ventana actual.
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // Cuando no se restaura la pila de navegación, navegar a la primera página,
+                    // configurando la nueva página pasándole la información requerida como
+                    //parámetro de navegación
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Asegurarse de que la ventana actual está activa.
+                Window.Current.Activate();
+            }
+        }
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+
+        /// <param name="sender">Origen de la solicitud de suspensión.</param>
+        /// <param name="e">Detalles sobre la solicitud de suspensión.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+
+            widgetCrosshair = null;
+
+            deferral.Complete();
+        }
+
+        private async void CenterWidgetOnLaunch()
+        {
+            // Verifica si el widget ya ha sido inicializado
+            if (widgetCrosshair != null)
+            {
+                // Llama a CenterWindowAsync para centrar la ventana del widget
+                await widgetCrosshair.CenterWindowAsync();
+            }
+        }
+    }
+}
---
+++ Crosshair/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml.Controls;
+using System;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Crosshair
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadCrosshair();
+        }
+
+        private void LoadCrosshair()
+        {
+            // Crear un BitmapImage desde los recursos
+            var crosshairImage = new BitmapImage(new Uri("ms-appx:///Assets/Crosshair2.png"));
+
+            // Asignar la imagen al control Image en la UI
+            CrosshairImage.Source = crosshairImage;
+        }
+    }
+}
